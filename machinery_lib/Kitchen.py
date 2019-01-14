cee956d3098d8097ff40812c7629ed8605008fde
vvvis <vvis@i.ua>
2019-01-14 23:10:44 +0200

    Addedd main functionality of kitchen and Cofffe Machine class.

--- machinery_lib/Kitchen.py
+++ machinery_lib/Kitchen.py
@@ -2,12 +2,54 @@ from .Small_Machinery import *
 
 
 class Kitchen:
-    coffee_machine = CoffeeMachine
 
-    def add_water(coffee_machine, water):
+    def __init__(self):
+        self.barista = CoffeeMachine()
+
+    def barista_get_status(self):
+        print(self.barista.get_water_tank_status()
+              + self.barista.get_milk_tank_status()
+              + self.barista.get_coffee_tank_status()
+              + self.barista.get_power_status())
+
+    def barista_power_switch(self):
+        self.barista.power_switch()
+        if self.barista.power == "on":
+            print('YEAH! NOW WE`RE COOCKING!')
+        else:
+            print('OH, C`MON (x_x)!')
+
+
+
+    def barista_add_water(self, water):
         try:
-            return coffee_machine.coffee_machine.add_water(coffee_machine,water)
-            print(self.machine.get_water_tank_status())
+            self.barista.add_water(water)
+            print('Water added! ' + self.barista.get_water_tank_status())
         except Exception as e:
             print(e)
 
+    def barista_add_coffee(self, coffee):
+        try:
+            self.barista.add_coffee(coffee)
+            print('Coffee Added! ' + self.barista.get_coffee_tank_status())
+        except Exception as e:
+            print(e)
+
+    def barista_add_milk(self, milk):
+        try:
+            self.barista.add_milk(milk)
+            print('Milk Added! ' + self.barista.get_milk_tank_status())
+        except Exception as e:
+            print(e)
+
+    def brew_espresso(self):
+        try:
+            self.barista.brew_espresso()
+        except Exception as e:
+            print(e)
+
+    def brew_latte(self):
+        try:
+            self.barista.brew_latte()
+        except Exception as e:
+            print(e)
--- main.py
+++ main.py
@@ -1,5 +1,61 @@
 from machinery_lib.Kitchen import *
 
-barista = Kitchen
-barista.add_water(barista,15)
-
+kitchen = Kitchen()
+hint = '''
+    ----------------------------------------------------------
+    |                                                        |
+    |    Welcome to SMART KITCHEN 9000!                      |
+    | Please, tell your desire, master!                      |
+    | Possible commands:                                     |
+    |   1) latte - Tasty latte for master!                   |
+    |   2) espresso - Strong espresso for master!            |
+    |   3) barista_switch - Toggle barista power switch      |
+    |   4) barista_milk - Refill milk                        |
+    |   5) barista_water - Refill water                      |
+    |   6) barista_coffee - refill coffee                    |
+    |   7) hint - get list of commands of SMART KITCHEN 9000 |
+    |   8) exit - Pretty self-explanatory :P                 |
+    ----------------------------------------------------------
+    '''
+print(hint)
+command_list = ['latte', 'espresso', 'barista_switch', 'barista_milk', 'barista_water',
+                'barista_coffee', 'hint', 'exit']
+while True:
+    try:
+        user_input = input()
+        if user_input not in command_list:
+            raise Exception('Sorry, Mater! I am not of understanding! Maybe "hint" command would help?')
+        if user_input == 'latte':
+            kitchen.brew_latte()
+        if user_input == 'espresso':
+            kitchen.brew_espresso()
+        if user_input == 'barista_switch':
+            kitchen.barista_power_switch()
+        if user_input == 'barista_milk':
+            print('How much, Master?')
+            try:
+                cups = int(input())
+                kitchen.barista_add_milk(int(cups))
+            except Exception as e:
+                print('Master! I need a number amount to refill!')
+        if user_input == 'barista_water':
+            print('How much, Master?')
+            try:
+                cups = int(input())
+                kitchen.barista_add_water(int(cups))
+            except Exception as e:
+                print('Master! I need a number amount to refill!')
+        if user_input == 'barista_coffee':
+            print('How much, Master?')
+            try:
+                cups = int(input())
+                kitchen.barista_add_coffee(int(cups))
+            except Exception as e:
+                print('Master! I need a number amount to refill!')
+        if user_input == 'hint':
+            print(hint)
+        if user_input == 'exit':
+            print('Bye, master!')
+            break
+    except Exception as e:
+        print(e)
